fdfa0e5fa630f0fce13b69718cff92d857ab848e
Vice Sallés <vicenc.salles@gmail.com>
2017-04-06 07:43:07 +0200

    AppCycle advances properly until 'Interview' state

--- adocs/src/components/VideoPlayer/VideoPlayer.jsx
+++ adocs/src/components/VideoPlayer/VideoPlayer.jsx
@@ -6,16 +6,24 @@ export default class VideoPlayer extends Component {
  constructor(props){
         super(props);
 
-        this.props.media.then(stream=>{
+     /*   this.props.media.then(stream=>{
             this.refs.video.srcObject = stream;
             this.refs.video.muted = true;
             this.refs.video.play();
-        })
-
-        
+        })*/
     }
 
-    render() {
+    componentDidMount(){
+
+        //Setting up the player
+         this.refs.video.srcObject = this.props.media;
+         this.refs.video.muted = true;
+         this.refs.video.play();
+
+
+    }
+
+    render() {        
      
         return (<section id="player">
             <video ref="video" id="video">The stream is not ready</video>
