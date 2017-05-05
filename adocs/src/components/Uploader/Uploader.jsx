be1b54f5090e1bca5cf089ea0d86ac9784e46e7a
Vice Sallés <vicenc.salles@gmail.com>
2017-05-05 15:54:08 +0200

    Setting up the uploader that I won't use soon

--- adocs/src/components/Uploader/Uploader.jsx
+++ adocs/src/components/Uploader/Uploader.jsx
@@ -1,39 +1,51 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ajax from 'superagent';
 
-export default class Uploader extends Component{
-    constructor(props){
+export default class Uploader extends Component {
+    constructor(props) {
         super(props);
-        this.state={'phase':'waiting','pct':0, 'gotCredentials':false};
-        this.upload=this.upload.bind(this);
+        this.state = {
+            'phase': 'waiting',
+            'pct': 0,
+            'gotCredentials': false
+        };
+        this.upload = this
+            .upload
+            .bind(this);
     }
-    componentDidMount(){
-        
-    }
-    render(){
+    componentDidMount() {}
+    render() {
         switch (this.state.phase) {
             case 'waiting':
-                
+
                 break;
-        
+
             case 'uploading':
-                
-                return(<canvas ref="upload" id="upload"></canvas>);
-                
-                break;    
-            
-            case 'error':
-                
+
+                return (
+                    <canvas ref="upload" id="upload"></canvas>
+                );
+
                 break;
-            
+
+            case 'error':
+
+                break;
+
             case 'done':
-                
+
                 break;
 
             default:
-                return(<div><p><b>UPLOADING</b> Waiting for response</p></div>);
+                return (
+                    <div>
+                        <p>
+                            <b>UPLOADING</b>
+                            Waiting for response</p>
+                    </div>
+                );
                 break;
         }
-        
+
     }
 }
