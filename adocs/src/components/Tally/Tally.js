cac6f30a5503c94c98dd269d8b9cb012bf8c3b0d
Vice Sallés <vicenc.salles@gmail.com>
2018-01-08 07:28:40 +0100

    feat: Tally

---
+++ adocs/src/components/Tally/Tally.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Recording.css';
+
+export default function Recording(props){
+    return (<div className="recording"><div className="recBall"></div> <span className="recText">Recording</span></div>);
+}
